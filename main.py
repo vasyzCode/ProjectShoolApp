35618b3e8a7bc58b417d8083297c7b2816564955
vasyz <vladyar334@mail.ru>
2022-10-18 19:46:44 +0500

    Commit!

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from kivy.config import Config
+# import android
 
 Config.set('graphics', 'width', 720)
 Config.set('graphics', 'height', 1280)
 Config.set('graphics', 'resizable', False)
 
+
 from kivy.app import App
+from kivymd.app import MDApp
 from kivy.properties import StringProperty
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 import os
@@ -13,45 +16,53 @@ from data.users import User
 
 from data.connected import Connected
 
-# global_init("sql.freedb.tech:3306/freedb_users_vasyz")
-# session = create_session()
-
-KV = '''
-Screen:
-
-    NavigationLayout:
-
-        ScreenManager:
-
-            Screen:
-
-                BoxLayout:
-                    orientation: 'vertical'
-
-                    MDToolbar:
-                        title: "Navigation Drawer"
-                        elevation: 10
-                        left_action_items: [['menu', lambda x: nav_drawer.toggle_nav_drawer()]]
-
-                    Widget:
-
-
-        MDNavigationDrawer:
-
-            ContentNavigationDrawer:
-'''
+global_init("sql.freedb.tech:3306/freedb_users_vasyz")
+session = create_session()
 
 
 class Login(Screen):
+
+    def on_focus(self, value):
+        if value:
+            print('User focused', self)
+        else:
+            print('User defocused', self)
+
     def do_login(self, loginText, passwordText, dsp):
         app = App.get_running_app()
         app.username = loginText
         app.password = passwordText
-        # users = session.query(User).filter(User.login == loginText).all()
-        # if users:
+        users = session.query(User).filter(User.login == loginText).all()
+        if users:
+            print("Find user")
+            user_ = users[0]
+            if user_.check_password(passwordText):
+                print("Correct password")
+                self.manager.transition = SlideTransition(direction="left")
+                self.manager.current = 'connected'
+                dsp.text = ""
+
+                app.config.read(app.get_application_config())
+                app.config.write()
+            else:
+                dsp.text = "Неверные данные"
+        else:
+            dsp.text = "Неверные данные"
+        # else:
+        #     print("No User")
+        #     dsp.text = "Неверные данные"
+        #     if input("create? : ") == 'y':
+        #         user_ = User()
+        #         user_.login = loginText
+        #         user_.name = loginText
+        #         user_.set_password(passwordText)
+        #         session.add(user_)
+        #         session.commit()
+        # user = input("Пользователь существует? : y : n : ")
+        # if user == 'y':
         #     print("Find user")
-        #     user_ = users[0]
-        #     if user_.check_password(passwordText):
+        #     correct_pass = input("Пароль верный? : y : n : ")
+        #     if correct_pass == 'y':
         #         print("Correct password")
         #         self.manager.transition = SlideTransition(direction="left")
         #         self.manager.current = 'connected'
@@ -64,30 +75,7 @@ class Login(Screen):
         #     print("No User")
         #     dsp.text = "Неверные данные"
         #     if input("create? : ") == 'y':
-        #         user_ = User()
-        #         user_.login = loginText
-        #         user_.name = loginText
-        #         user_.set_password(passwordText)
-        #         session.add(user_)
-        #         session.commit()
-        user = input("Пользователь существует? : y : n : ")
-        if user == 'y':
-            print("Find user")
-            correct_pass = input("Пароль верный? : y : n : ")
-            if correct_pass == 'y':
-                print("Correct password")
-                self.manager.transition = SlideTransition(direction="left")
-                self.manager.current = 'connected'
-
-                app.config.read(app.get_application_config())
-                app.config.write()
-            else:
-                dsp.text = "Неверные данные"
-        else:
-            print("No User")
-            dsp.text = "Неверные данные"
-            if input("create? : ") == 'y':
-                print("create user")
+        #         print("create user")
                 # user_ = User()
                 # user_.login = loginText
                 # user_.name = loginText
@@ -98,6 +86,8 @@ class Login(Screen):
     def resetForm(self):
         self.ids['login'].text = ""
         self.ids['password'].text = ""
+
+
 
 
 class LoginApp(App):
@@ -124,7 +114,6 @@ class LoginApp(App):
         return super(LoginApp, self).get_application_config(
             '%s/config.cfg' % (conf_directory)
         )
-
 
 if __name__ == '__main__':
     LoginApp().run()
